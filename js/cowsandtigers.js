d375539ee25a2c5a2f4f8cbfc8016c83f51c2143
Leva <levon.oganesov@mail.ru>
2018-03-13 23:12:41 +0300

    show objects on map in html

--- js/cowsandtigers.js
+++ js/cowsandtigers.js
@@ -1,9 +1,8 @@
-var cowsandtigers = (function ($) {
+var cowsandtigers = (function () {
     var devMode = false;
 
     // OBJ GAME
-    function Game($gameContainer, setting) {
-        this.$gameContainer = $gameContainer;
+    function Game(setting) {
         this.setting = setting;
         // --------------
         // Установим режим игры
@@ -12,123 +11,147 @@ var cowsandtigers = (function ($) {
         // Установим скорость игрового цикла
         this.timeRender = this.setting.timeRender || 500;
 
-        var scene = new Scene(this.setting);
-
-        scene.build();
-
-        // this.run();
+        // Создадим новую сцену
+        this.scene = new Scene(this.setting);
     }
 
     // GAME LOOP
     Game.prototype.run = function () {
+        // Создадим игровое поле на сцене
+        this.scene.build();
+
         if (!devMode) {
             // Главный Loop
-            setInterval(function () {
-                // scene.event(map);
-                // scene.render(map);
+            setInterval(() => {
+                this.scene.actionOnMap();
+                this.scene.render();
             }, this.timeRender);
         } else {
-            // scene.event(map);
-            // scene.render(map);
+            this.scene.actionOnMap();
+            this.scene.render();
         }
     };
 
-    // Основной Прототип объекта на карте
-    function ObjectOnMap(className, id, setting) {
-        this.id = id;
-        this.className = className;
-        this.objectPositionRowCol = [];
-
-        this.min = setting.mapObjects[this.className]["min"];
-        this.max = setting.mapObjects[this.className]["max"];
-
-        console.log(this);
-    }
-    ObjectOnMap.prototype.show = function () {
-        return "<div class='"+this.className+"'></div>";
-    };
-    ObjectOnMap.prototype.action = function () {
-        console.log('prototype action: ' + this.className);
-    };
-    // ------------------------------------------
-
+    // Игровая сцена
     function Scene(setting) {
-        this.gameContainer = null;
-        this.mapObjects = [];
-
+        this.gameContainer = document.getElementById(setting.gameContainerID);
         this.map = new Map(setting);
     }
-
+    /**
+     * Проинициализируем карту и заполним ее объектами
+     */
     Scene.prototype.build = function () {
         this.map.init();
-
-        // var ground = new ObjectOnMap("ground", 0, this.setting);
-        // var grass = new ObjectOnMap("grass", 3, this.setting);
-        // var cows = new ObjectOnMap("cows", 1, this.setting);
-        // var tigers = new ObjectOnMap("tigers", 2, this.setting);
-
-        // // Добавим объекты в массив
-        // this.mapObjects.push(ground,cows,tigers,grass);
-
         this.map.generate();
+    };
+    /**
+     * Отрисовка заполненной карты
+     */
+    Scene.prototype.render = function () {
+        var $gameContainer = this.gameContainer;
+        var mapHTML = '';
 
-        //
-        //
-        // // Добавим траву
-        // map.generate(this.elements[3]);
-        // // Добавим коров
-        // map.generate(this.elements[1]);
-        // // Добавим тигров
-        // map.generate(this.elements[2]);
-        // // Добавим землю
-        // map.generate(this.elements[0]);
+        // Построим игровое поле
+        for (var row = 0; row < this.map.row; row++) {
+
+            mapHTML += "<div class='row'>";
+
+            for (var col = 0; col < this.map.col; col++) {
+
+                mapHTML += "<div class='cell'> " + this.map.mapData[row][col].show() + "</div>";
+
+            }
+
+            mapHTML += "</div>";
+        }
+
+        // Добавим собранную в html карту в DOM
+        $gameContainer.querySelectorAll('.plain')[0].innerHTML = mapHTML;
+    };
+    Scene.prototype.actionOnMap = function () {
+      console.log("Show action on Map in one time");
     };
     // ------------------------------------------
 
+    // Прототип работы с картой
     function Map (setting) {
-        this.setting = setting;
         this.mapData = [];
+        this.mapObjects = setting.mapObjects;
+        this.objectsOnMap = [];
 
-        this.row = this.setting.map.row || 0;
-        this.col = this.setting.map.col || 0;
+        this.row = setting.mapSize.row || 0;
+        this.col = setting.mapSize.col || 0;
     }
-
+    /**
+     * Построим пустую карту на основе начальных Row/Col
+     */
     Map.prototype.init = function () {
         while(this.mapData.push([]) < this.row);
     };
-
+    /**
+     * Генерация карты
+     */
     Map.prototype.generate = function () {
-        var mapObjects = this.setting.mapObjects;
+        // object ID
+        var objID = 0;
 
-        console.log(mapObjects);
+        for(var objectName in this.mapObjects ) {
+            var isStaticObject = false;
 
+            var objMin = this.mapObjects[objectName].min;
+            var objMax = this.mapObjects[objectName].max;
 
+            // Если объект являеться статикой,
+            // то постаремся дать по максимуму значения min/max
+            // для полного заполнения игрового поля
+            if (objMin === null && objMax === null) {
+                objMin = (this.row + this.col) * 2;
+                objMax = (this.row + this.col) * 100;
 
-        // Получим кол-во объектов на карте
-        var objCountOnMap = tools.randomInteger(obj.min, obj.max);
-
-        // console.log(objCountOnMap);
-
-        // Пройдемся по этому количевству
-        for (var i = 0; i < objCountOnMap; i++) {
-
-            var mapRowCol = this.getRandomRowColCoord();
-
-            // console.log('mapRowColNormal: ', mapRowCol);
-
-            if (this.mapData[mapRowCol.row][mapRowCol.col] == undefined) {
-                this.mapData[mapRowCol.row][mapRowCol.col] = obj.id;
-
-                // Сохраним координаты добавленного элемента на карте
-                obj.saveCoordinate(mapRowCol.row,mapRowCol.col);
-
-            } else {
-                this.tryNewGenerate(obj, objCountOnMap);
+                isStaticObject = true;
             }
+
+            // Получим кол-во объектов на карте
+            var objCountOnMap = tools.randomInteger(objMin, objMax);
+
+            // console.log(objCountOnMap);
+
+            // Пройдемся по этому количевству
+            for (var i = 0; i < objCountOnMap; i++) {
+
+                var mapRowCol = this.getRandomRowColCoord();
+
+                // console.log('mapRowColNormal: ', mapRowCol);
+
+                if (this.mapData[mapRowCol.row][mapRowCol.col] == undefined) {
+                    var object = new ObjectOnMap(objectName, objID, mapRowCol.row, mapRowCol.col);
+
+                    this.mapData[mapRowCol.row][mapRowCol.col] = object;
+
+                    if (!isStaticObject) {
+                        this.objectsOnMap.push(object);
+                    }
+                } else {
+                    var objectSetting = {
+                        objID: objID,
+                        objectName: objectName,
+                        isStaticObject: isStaticObject
+                    };
+                    this.tryNewGenerate(objectSetting, objCountOnMap);
+                }
+            }
+
+            objID++;
         }
     };
-
-    Map.prototype.tryNewGenerate = function (obj, count) {
+    /**
+     * Повторная генерация, в случии занятого места в массиве
+     * @param objectName
+     * @param objID
+     * @param count
+     * @returns {boolean}
+     */
+    Map.prototype.tryNewGenerate = function (objectSetting, count) {
 
         if(count <= 0) return false;
 
@@ -141,17 +164,25 @@ var cowsandtigers = (function ($) {
             // console.log('mapRowColRecursive: ', mapRowCol);
 
             if (this.mapData[mapRowCol.row][mapRowCol.col] == undefined) {
-                this.mapData[mapRowCol.row][mapRowCol.col] = obj.id;
+                var object = new ObjectOnMap(objectSetting.objectName, objectSetting.objID, mapRowCol.row, mapRowCol.col);
 
-                // Сохраним координаты добавленного элемента на карте
-                obj.saveCoordinate(mapRowCol.row,mapRowCol.col);
+                this.mapData[mapRowCol.row][mapRowCol.col] = object;
+
+                if (!objectSetting.isStaticObject) {
+                    this.objectsOnMap.push(object);
+                }
+
                 return false;
             } else {
-                return this.tryNewGenerate(obj, count - 1);
+                var objectSetting = {
+                    objID: objectSetting.objID,
+                    objectName: objectSetting.objectName,
+                    isStaticObject: objectSetting.isStaticObject
+                };
+                return this.tryNewGenerate(objectSetting, count - 1);
             }
         }
     };
-
     /**
      * Получим произвольные координаты на основе кол-во строк и колонок
      * @param setting
@@ -166,33 +197,37 @@ var cowsandtigers = (function ($) {
             col: tools.randomInteger(0, countCol)
         }
     };
+    // ------------------------------------------
 
+    // Основной Прототип объекта на карте
+    function ObjectOnMap(className, id, objPositionRow, objPositionCol) {
+        this.id = id;
+        this.className = className;
+        this.objectPositionRow = objPositionRow || 0;
+        this.objectPositionCol = objPositionCol || 0;
+
+        // console.log(this);
+    }
+    /**
+     * Вывод HTML объекта
+     * @returns {string}
+     */
+    ObjectOnMap.prototype.show = function () {
+        return "<div class='"+this.className+"'></div>";
+    };
+    /**
+     * Разные действия объекта
+     */
+    ObjectOnMap.prototype.action = function () {
+        console.log('prototype action: ' + this.className);
+    };
     // ------------------------------------------
 
     // Вспомогательные функции для игры
     var tools = {
-
         randomInteger: function (min, max) {
             return Math.floor(Math.random() * (max - min)) + min;
         }
-        /*
-        showLoading: function (startNumber, endNumber, $gameContainer, process) {
-            var progress = startNumber * 100 / endNumber;
-            var $progressBar = $gameContainer.find('progress');
-            var $process = $gameContainer.find('.process');
-
-            $progressBar.show();
-            $process.show();
-
-            $progressBar.val(progress);
-            $process.html(process);
-
-            if (startNumber == (endNumber -1)) {
-                $progressBar.hide();
-                $process.hide();
-            }
-        }
-        */
     };
     // ------------------------------------------
 
@@ -201,7 +236,7 @@ var cowsandtigers = (function ($) {
         init: function ($gameContainer, setting) {
             var game = new Game($gameContainer, setting);
 
-            game.init();
+            game.run();
         }
     }
-})(jQuery);
+})();
